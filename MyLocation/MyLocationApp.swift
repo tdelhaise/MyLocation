d408ae286ecfee761540b167b277469d44f553a7
Thierry DELHAISE <t.delhaise@partnre.com>
2023-07-19 13:13:10 +0200

    Initial Commit

---
+++ MyLocation/MyLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationApp.swift
+//  MyLocation
+//
+//  Created by THIERRY DELHAISE on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
